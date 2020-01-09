010cbb84b6fadf0aaa5261ed24e7f8a586483835
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-09 09:31:38 -0600

    Customer Register Page Error Checking

--- WebContent/scripts/customer-registration.js
+++ WebContent/scripts/customer-registration.js
@@ -6,23 +6,31 @@ let inputTextUserName = document.getElementById("user-name-registration");
 let inputPasswordOne = document.getElementById("password-one-registration");
 let inputPasswordTwo = document.getElementById("password-two-registration");
 
+// Get Alert For User Cerdential Verification
+let alertInvalidUserName = document.getElementById("alert-invalid-username");
+let alertInvalidPassword = document.getElementById("alert-invalid-password");
+let alertPasswordMismatch = document.getElementById("alert-password-mismatch");
+
+
 // Add Event Listener For Form Registration
 formCustomerRegsitration.addEventListener("submit", (e) => {
-    e.preventDefault();// for testing remove for after adding backend support
     let userName = inputTextUserName.value.trim();
     let passwordOne = inputPasswordOne.value;
     let passwordTwo = inputPasswordTwo.value;
     
-    if(validateInput(userName, 6)){
-        console.log("user name is valid");
-    }else{
-        console.log("user name is invalid");
+    if(!validateInput(userName, 5)){
+        showInvalidUserNameAlert();
+        e.preventDefault();
     }
     let matchingPasswords = passwordsMatch(passwordOne, passwordTwo);
-    if(matchingPasswords && validateInput(passwordOne, 5)){
-        console.log("password is valid.");
-    }else{
-        console.log("password is invalid.");
+    if(!validateInput(passwordOne, 3)){
+        showInvalidPasswordAlert();
+        e.preventDefault();
+    }
+
+    if(!matchingPasswords){
+        showPasswordMismatch();
+        e.preventDefault();
     }
 });
 
@@ -38,4 +46,34 @@ function passwordsMatch(passwordA, passwordB){
 function validateInput(input, minLength){
     let pattern = new RegExp(`^[a-zA-Z0-9]{${minLength},}$`, "g");
     return input.match(pattern);
+}
+
+// Show Alerts For Invalid user names
+function showInvalidUserNameAlert(){
+    if(alertInvalidUserName.classList.contains("d-none")){
+        alertInvalidUserName.classList.remove("d-none");
+        setTimeout( () => {
+            alertInvalidUserName.classList.add("d-none");
+        }, 7000 );
+    }
+}
+
+// Show Alerts For Invalid user names
+function showInvalidPasswordAlert(){
+    if(alertInvalidPassword.classList.contains("d-none")){
+        alertInvalidPassword.classList.remove("d-none");
+        setTimeout( () => {
+            alertInvalidPassword.classList.add("d-none");
+        }, 7000 );
+    }
+}
+
+// Show Password Mismatch
+function showPasswordMismatch(){
+    if(alertPasswordMismatch.classList.contains("d-none")){
+        alertPasswordMismatch.classList.remove("d-none");
+        setTimeout( () => {
+            alertPasswordMismatch.classList.add("d-none");
+        }, 7000 );
+    }
 }
