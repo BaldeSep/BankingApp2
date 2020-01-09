b99d4fa60b873204c8028d85bb96927fc410c33b
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-09 09:12:21 -0600

    Client Side Verification Of UserName and Password

--- WebContent/scripts/login.js
+++ WebContent/scripts/login.js
@@ -5,11 +5,22 @@ let formCustomer = document.getElementById("login-form");
 let inputUserName = document.getElementById("user-name");
 let inputPassword = document.getElementById("password");
 
+// Get Alerts For Invalid User Credentials
+let alertUserName = document.getElementById("alert-invalid-username");
+let alertPassword = document.getElementById("alert-invalid-password");
+
 // Add Event Listener To Form
 formCustomer.addEventListener("submit", (e) => {
     let userName = inputUserName.value.trim();
     let password = inputPassword.value.trim();
-    // Post this to the server to verify user credientials
+    if(!verify(userName, 5)){
+        showInvalidUserName();
+        e.preventDefault();
+    }
+    if(!verify(password, 3)){
+        showInvalidPassword();
+        e.preventDefault();
+    }
 });
 
 
@@ -17,4 +28,24 @@ formCustomer.addEventListener("submit", (e) => {
 function verify(input, minLength){
     let pattern = new RegExp(`^[A-Za-z0-9]{${minLength},}$`, "g");
     return input.match(pattern);
+}
+
+
+// Show Invalid User Name Alert
+function showInvalidUserName(){
+    if(alertUserName.classList.contains("d-none")){
+        alertUserName.classList.remove("d-none");
+        setTimeout( () => {
+            alertUserName.classList.add("d-none");
+        },3000 );
+    }
+}
+// Show Invalid Password Alert
+function showInvalidPassword(){
+    if(alertPassword.classList.contains("d-none")){
+        alertPassword.classList.remove("d-none");
+        setTimeout( () => {
+            alertPassword.classList.add("d-none");
+        },3000 );
+    }
 }
